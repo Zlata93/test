5e7a980f07138342e20d56605a5f66488127bb6f
Zlata Kotlova <z.kotlova@informika.ru>
2019-09-23 15:55:26 +0300

    build

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -6,7 +6,6 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'production',
-    // devtool: 'source-map',
     module: {
         rules: [{
             test: /\.js$/,
